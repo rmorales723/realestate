6421b04fd9ddb91abcef24742a96655ce90cf177
robert morales <rmoralfit@gmail.com>
2022-07-02 11:14:25 -0400

    properties displaying & started navbar

--- components/property.jsx
+++ components/property.jsx
@@ -14,15 +14,25 @@ const Property = ({property: {coverPhoto, price, rentFrequency, rooms, title, ba
             <Box>
                 <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} alt="house" />
             </Box>
-            <Box w="full">
-                <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
-                    <Flex alignItems="center">
-                        <Box paddingRight="3" color="green.400">{isVerified && <GoVerified />}</Box>
-                        <Text fontWeight='bold' fontSize='lg'>USD {price}{rentFrequency && `/${rentFrequency}`}</Text>
-                    </Flex>
-                </Flex>
-            </Box>
+            <Box w='full'>
+        <Flex paddingTop='2' alignItems='center' justifyContent='space-between'>
+          <Flex alignItems='center'>
+            <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified />}</Box>
+            <Text fontWeight='bold' fontSize='lg'>AED {price}{rentFrequency && `/${rentFrequency}`}</Text>
+          </Flex>
+          <Box>
+            <Avatar size='sm' src={agency?.logo?.url}></Avatar>
+          </Box>
+        </Flex>
+        <Flex alignItems='center' p='1' justifyContent='space-between' w='250px' color='blue.400'>
+          {rooms}
+          <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
+        </Flex>
+        <Text fontSize='lg'>
+          {title.length > 30 ? title.substring(0, 30) + '...' : title}
+        </Text>
+      </Box>
         </Flex>
     </Link>
-)
+);
 export default Property;
