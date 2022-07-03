ca44a32b4ced5c57d1f663656f99baff054bf537
robert morales <rmoralfit@gmail.com>
2022-07-02 20:14:50 -0400

    working on externalID (destructured but notpassing property)

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -4,6 +4,7 @@ import { Box, Icon, Flex } from '@chakra-ui/react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
 
+
 const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
 
@@ -35,14 +36,25 @@ const RightArrow = () => {
     </Flex>
   );
 }
-export default function ImageSrollbar({ data }) {
-  return (
+
+const ImageScrollbar = ({ data }) =>  (
+  
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: 'hidden' }} >
       {data.map((item) => (
-        <Box width='910px' itemId={item.id} overflow='hidden' p='1'>
-          <Image placeholder="blur" blurDataURL={item.url} src={item.url} width={1000} height={500}  sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px" />
+        <Box key={item.id} width='910px' itemId={item.id} overflow='hidden' p='1'>
+          <Image
+          alt="property" 
+          placeholder="blur" 
+          blurDataURL={item.url} 
+          src={item.url} 
+          width={1000} 
+          height={500}  
+          sizes="(max-width: 500px) 100px, (max-width): 1023px 400px, 1000px" 
+          />
         </Box>
       ))}
     </ScrollMenu>
   );
-}
+
+
+export default ImageScrollbar;
